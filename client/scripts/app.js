7ad307eeb874598806ec2e2aba09ac498d81dc56
tetondan <dfrehner1@gmail.com>
2015-12-07 19:53:37 -0800

    all tests pass, messages can be viewed no duplicates, XSS have been removed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,95 @@
 // YOUR CODE HERE:
+//https://api.parse.com/1/classes/chatterbox
+var app = {
+  init: function(){
+    $(".username").on("click", app.addFriend());
+    $("#send .submit").on("submit", app.handleSubmit());
+    
+  },
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  send: function(message){
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function(){
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      data: 'jsonp',
+      contentType: 'application/json',
+      success: function (data) {
+        app.args = data.results;
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to retrieve');
+      }
+    });
+  },
+  userList: {},
+  objIdList: {},
+
+  addMessage: function(message){
+    if(app.objIdList[message.objectId] === undefined){
+      var name = app.rejector(message.username);
+      var post = app.rejector(message.text);
+      var room = app.rejector(message.roomname)
+      var $chatbox = $('<div class="chatbox"></div>');
+      $chatbox.append('<div class="username">'+name+' :</div>');
+      $chatbox.append('<div class="message">'+post+'</div>');
+      $chatbox.append('<div class="room">'+room+'</div>');
+      $('#chats').prepend($chatbox);
+      if(app.userList[name] === undefined && name !== undefined ){  
+        $('.users').append('<option value='+name+'>'+name+'</option>')
+        app.userList[name] = name;
+      }
+      app.objIdList[message.objectId] = message.objectId;
+    }    // $(".username").on( "click", function(){app.addFriend()} );
+  },
+
+  addRoom: function(roomName){
+    var $room = $('<div class="room">'+ roomName +'</div>');
+    $("#roomSelect").append($room);
+  },
+
+  addFriend: function(){
+    
+  },
+  handleSubmit: function(){
+    // app.send('message')
+  },
+  clearMessages: function(){
+    var myNode = document.getElementById("chats");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+    }
+  },
+  rejector: function(string){
+    return string.replace(/[<>&%*+^$;{}()|[\]\\]/g, "");
+  }
+  
+}
+// var userList = {},
+
+app.init();
+setInterval(function(){
+    app.fetch();
+    for(var i = 0; i<app.args.length; i++){
+      app.addMessage(app.args[i]);
+    }
+  }, 5000)
+
+
+
+
